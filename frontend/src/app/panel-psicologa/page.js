add439b8ba87864995f089679243d6a4c84bb761
luu2333 <nataliaballs19@gmail.com>
2025-12-06 11:11:08 -0500

    fix: rename vistas-reutilizables folder for vercel build

--- frontend/src/app/panel-psicologa/page.js
+++ frontend/src/app/panel-psicologa/page.js
@@ -13,7 +13,7 @@ import GraficaUsoChatbot from "./GraficaUsoChatbot";
 import GraficaEmociones from "./GraficaEmociones";
 
 import AlertasView from "./Alertas/AlertasView";
-import FrasesRiesgoView from "./frases/FrasesRiesgoView";
+import FrasesRiesgoView from "./Frases/FrasesRiesgoView";
 import SettingsView from "../vistas-reutilizables/SettingsView";
 import GraficaAlertasCriticas from "./GraficaAlertasCriticas";
 import ContenidoView from "./Contenido/ContenidoView";
